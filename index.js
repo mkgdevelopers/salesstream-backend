74a2930631513dd483951334fabf2757ad8afb06
mkgdevelopers <muaaznaeemoff@gmail.com>
2025-06-04 10:04:45 +0500

    cros origin fix

--- index.js
+++ index.js
@@ -7,8 +7,21 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middlewares
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://salesstream.vercel.app"
+];
+
 app.use(cors({
-  origin:'http://localhost:5173 , https://salesstream.vercel.app/'
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
 }));
 app.use(express.json());
 
